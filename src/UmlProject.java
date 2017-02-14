780b15106a9c332b8642dbf7063cdbdd0caeb0fa
dchuong <derrick@derricks-mbp.attlocal.net>
2017-02-13 18:57:35 -0800

    Done getting files
    Got all files from one directory

--- src/UmlProject.java
+++ src/UmlProject.java
@@ -1,6 +1,21 @@
-
+import java.io.File;
 public class UmlProject {
-	  public static void main(String[] args) {
-	        System.out.println("Hello"); 
-	    }
+	public static void main(String[] args) {
+        String Input = null, Output = null;
+        if (args.length == 2) {
+        	try {
+        		Input = args[0];
+        		Output = args[1];
+        	} catch (Exception e) {
+        		return;
+        	}
+        }
+        
+        System.out.println(Input + " " + Output + "\n");
+        File[] files = new File(Input).listFiles();
+        
+        for (File oneFile : files) {
+        	System.out.println(oneFile);
+        }
+    }
 }
