8a397b3f3965cff3843e64d3af4ef0996d5b9d89
dchuong <derrick@Derricks-MacBook-Pro.local>
2017-04-08 09:19:10 -0700

    added test cases

---
+++ umlparser/uml-parser-test-1/A.java
@@ -0,0 +1,18 @@
+    
+
+import java.util.Collection;
+
+public class A {
+ 
+	private int x;
+	 
+	public int[] y;
+	 
+	private Collection<B> b;
+	 
+	private C c;
+	 
+	private Collection<D> d;
+	 
+}
+ 
---
+++ umlparser/uml-parser-test-1/D.java
@@ -0,0 +1,8 @@
+ 
+
+public class D {
+ 
+	public A a;
+	 
+}
+ 
